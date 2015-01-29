8c6b650029c94342b8f7b02790e2942bd60b6bce
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-01-29 21:09:51 +0100

    Jason method bind

--- src/main/java/de/tu_clausthal/in/mec/object/mas/jason/actions/CMethodInvoke.java
+++ src/main/java/de/tu_clausthal/in/mec/object/mas/jason/actions/CMethodInvoke.java
@@ -24,19 +24,17 @@
 package de.tu_clausthal.in.mec.object.mas.jason.actions;
 
 
-import de.tu_clausthal.in.mec.common.CCommon;
-import jason.asSyntax.*;
+import jason.asSyntax.Structure;
+import jason.asSyntax.Term;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.*;
 
 
 /**
- * action to synchronize bind-object with agent value
+ * action to invoke any method on an object
  */
-public class CFieldBind extends IAction
+public class CMethodInvoke extends IAction
 {
 
     /**
@@ -44,10 +42,11 @@ public class CFieldBind extends IAction
      */
     protected Map<String, ImmutablePair<Object, Set<String>>> m_bind = new HashMap();
 
+
     /**
      * ctor - default
      */
-    public CFieldBind()
+    public CMethodInvoke()
     {
 
     }
@@ -55,10 +54,10 @@ public class CFieldBind extends IAction
     /**
      * ctor bind an object
      *
-     * @param p_name name of the binding object
+     * @param p_name   name of the binding object
      * @param p_object object
      */
-    public CFieldBind( String p_name, Object p_object )
+    public CMethodInvoke( String p_name, Object p_object )
     {
         m_bind.put( p_name, new ImmutablePair<Object, Set<String>>( p_object, new HashSet() ) );
     }
@@ -104,13 +103,12 @@ public class CFieldBind extends IAction
     @Override
     public String getName()
     {
-        return "pushfield";
+        return "invokemethod";
     }
 
     @Override
     public void act( Structure p_args )
     {
-
         // check number of argument first
         List<Term> l_args = p_args.getTerms();
         if ( l_args.size() < 3 )
@@ -122,25 +120,13 @@ public class CFieldBind extends IAction
         if ( l_object == null )
             throw new IllegalArgumentException( "object key [" + l_objectname + "] not found" );
 
-        String l_fieldname = l_args.get( 1 ).toString();
+        String l_methodname = l_args.get( 1 ).toString();
         try
         {
 
             for ( String l_name : l_object.getRight() )
-                if ( l_name.equals( l_fieldname ) )
-                    throw new IllegalAccessException( "field [" + l_fieldname + "] not accessible for the object [" + l_objectname + "]" );
-
-            Field l_field = CCommon.getClassField( l_object.getLeft().getClass(), l_fieldname );
-            if ( ( Modifier.isFinal( l_field.getModifiers() ) ) || ( Modifier.isAbstract( l_field.getModifiers() ) ) || ( Modifier.isInterface( l_field.getModifiers() ) ) || ( Modifier.isStatic( l_field.getModifiers() ) ) )
-                throw new IllegalAccessException( "abstract, final, static or interface field [" + l_fieldname + "] at object [" + l_objectname + "] cannot be set" );
-
-            if ( l_args.get( 2 ).isNumeric() )
-                l_field.set( l_object.getLeft(), l_field.getType().cast( ( (NumberTerm) l_args.get( 2 ) ).solve() ) );
-
-            if ( l_args.get( 2 ).isString() )
-                l_field.set( l_object.getLeft(), ( (StringTerm) l_args.get( 2 ) ).getString() );
-
-            // @todo term list
+                if ( l_name.equals( l_methodname ) )
+                    throw new IllegalAccessException( "method [" + l_methodname + "] not accessible for the object [" + l_objectname + "]" );
 
         }
         catch ( Exception l_exception )

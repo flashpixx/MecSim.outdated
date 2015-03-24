d81c8d1fe0fd701c4db2a2c8a73e6d224270839b
Philipp Kraus <pkraus@philipp-desktop.in.tu-clausthal.de>
2015-03-24 14:45:00 +0100

    remove markdown css

--- src/main/java/de/tu_clausthal/in/mec/ui/web/CMarkdownRenderer.java
+++ src/main/java/de/tu_clausthal/in/mec/ui/web/CMarkdownRenderer.java
@@ -189,8 +189,7 @@ public class CMarkdownRenderer extends LinkRenderer
      */
     private String getHTMLDocument( final PegDownProcessor p_processor, final URL p_file ) throws IOException
     {
-        return "<?xml version=\"1.0\" ?>\n" +
-                "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
+        return "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
                 "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 ( m_cssuri.isEmpty() ? "" : "<head><link rel=\"stylesheet\" type=\"text/css\" href=\"" + m_cssuri + "\"/></head>\n" ) +
                 "<body>\n" + this.getHTMLFragment( p_processor, p_file ) + "\n</body></html>";

56994d02dce91910a537cbf3879ea5621b51c4a7
Thomas Hornoff <thomas.hornoff@gmail.com>
2015-03-25 15:03:35 +0100

    refactor web ui buttons

--- src/main/resources/web/root/jscss/main.js
+++ src/main/resources/web/root/jscss/main.js
@@ -4,29 +4,27 @@ $(document).ready(function(){
      * call to set template links
      * @see http://www.html5rocks.com/en/tutorials/webcomponents/imports/?redirect_from_locale=de
     **/
-    $("a.template").click(function( p_event ){
+    $("a.template").button().click(function( p_event ){
         p_event.preventDefault();
         $("#mecsim_content").load( this.href );
     });
 
-
     $( "#mecsim_accordion" ).accordion();
 
 
-    $( "#mecsim_start_button" ).click(function() {
+    $( "#mecsim_start_button" ).button().click(function() {
         $.post(
             "csimulation/start"
         );
     });
 
-
-    $( "#mecsim_stop_button" ).click(function() {
+    $( "#mecsim_stop_button" ).button().click(function() {
         $.post(
             "csimulation/stop"
         );
     });
 
-
     $('#jqxSplitter').jqxSplitter({ width: '100%', height: '100%', panels: [{ size: '20%', min: 250 }, { size: '80%'}] });
+
 
 });

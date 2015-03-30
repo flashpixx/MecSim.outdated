a8ec304631f1b7f23349b46919bf0b23871e22fe
Thomas Hornoff <thomas.hornoff@gmail.com>
2015-03-30 15:02:30 +0200

    add configuration structure | refactor accordion

--- src/main/resources/web/root/jscss/main.js
+++ src/main/resources/web/root/jscss/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+    // TODO: add button text according to selected language
+    /*$('#mecsim_user_documentation').text(function() {
+        $.post(
+            ""
+        );
+    });*/
+
     /**
      * call to set template links
      * @see http://www.html5rocks.com/en/tutorials/webcomponents/imports/?redirect_from_locale=de
@@ -15,7 +22,10 @@ $(document).ready(function(){
         location.reload();
     });
 
-    $( "#mecsim_accordion" ).accordion();
+    $( "#mecsim_accordion" ).accordion({
+        active: false,
+        collapsible: true
+    });
 
     $( "#mecsim_start_button" ).button().click(function() {
         $.post(
@@ -30,6 +40,5 @@ $(document).ready(function(){
     });
 
     $('#jqxSplitter').jqxSplitter({ width: '100%', height: '100%', panels: [{ size: '20%', min: 250 }, { size: '80%'}] });
-
 
 });

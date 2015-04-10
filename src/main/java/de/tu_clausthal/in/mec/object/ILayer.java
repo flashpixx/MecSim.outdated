db275257d605ebce9a16d59ff64581eab42341b6
Philipp Kraus <pkraus@philipp-desktop.in.tu-clausthal.de>
2015-04-10 14:36:27 +0200

    fix styleguide

--- src/main/java/de/tu_clausthal/in/mec/object/ILayer.java
+++ src/main/java/de/tu_clausthal/in/mec/object/ILayer.java
@@ -19,7 +19,7 @@
  * # along with this program. If not, see http://www.gnu.org/licenses/                  #
  * ######################################################################################
  * @endcond
- */
+ **/
 
 package de.tu_clausthal.in.mec.object;
 
--- src/main/java/de/tu_clausthal/in/mec/object/source/sourceTarget/CAtomTarget.java
+++ src/main/java/de/tu_clausthal/in/mec/object/source/sourceTarget/CAtomTarget.java
@@ -1,5 +1,6 @@
 /**
- * @cond ######################################################################################
+ * @cond LICENSE
+ * ######################################################################################
  * # GPL License                                                                        #
  * #                                                                                    #
  * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
@@ -18,8 +19,8 @@
  * # along with this program. If not, see http://www.gnu.org/licenses/                  #
  * ######################################################################################
  * @endcond
- */
-//test
+ **/
+
 package de.tu_clausthal.in.mec.object.source.sourcetarget;
 
 import de.tu_clausthal.in.mec.ui.COSMViewer;

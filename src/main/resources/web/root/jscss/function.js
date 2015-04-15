dd951ff9eebb2fbe55deda8c1d6902fbebafd6a0
Philipp Kraus <pkraus@philipp-desktop.in.tu-clausthal.de>
2015-04-15 08:42:04 +0200

    add string prototype

--- src/main/resources/web/root/jscss/function.js
+++ src/main/resources/web/root/jscss/function.js
@@ -1,3 +1,14 @@
+// --- global prototype functions ------------------------------------------------------------------------------------------------------------------------------
+String.prototype.startsWith = function(prefix) {
+    return this.indexOf(prefix) === 0;
+}
+
+String.prototype.endsWith = function(suffix) {
+    return this.match(suffix+"$") == suffix;
+};
+
+
+// --- jQuery --------------------------------------------------------------------------------------------------------------------------------------------------
 // @warn need jQuery & jQuery-UI
 // @see http://learn.jquery.com/jquery-ui/widget-factory/why-use-the-widget-factory/
 (function($){
@@ -14,8 +25,8 @@
 
 
         // public member
-        websocket : function( pc_relativepath ) {
-            return new WebSocket( ((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.hostname + (((window.location.port != 80) && (window.location.port != 443)) ? ":" + window.location.port : "") + window.location.pathname + pc_relativepath );
+        websocket : function( wspath ) {
+            return new WebSocket( ((location.protocol === "https:") ? "wss://" : "ws://") + location.hostname + (((location.port != 80) && (location.port != 443)) ? ":" + location.port : "") + (wspath.startsWith("/") ? wspath : location.pathname + wspath ) );
         },
 
 
@@ -26,3 +37,6 @@
     });
 
 })(jQuery);
+
+
+

b8ddb4056b3e181cc79da1b0cd409eab22171265
Philipp Kraus <pkraus@imac.local>
2015-04-23 23:48:40 +0200

    add switches

--- src/main/resources/web/root/jscss/main.js
+++ src/main/resources/web/root/jscss/main.js
@@ -64,10 +64,16 @@ $.when(
         url     : "/csimulation/listlayer",
         success : function( px_data ){
             $.each( px_data, function( pc_key, px_value ) {
-                $( "#mecsim_simulationlayer" ).append( "<li class=\"ui-widget-content\" id=\"" + px_value.id + "\">" + pc_key + "</li>" );
+                console.log(px_value)
+                $( "#mecsim_simulationlayer" ).append( "<li class=\"ui-widget-content onoffswitch\" id=\"" + px_value.id + "\">"+ pc_key +
+                                                       "<input class=\"active\" type=\"checkbox\" " + (px_value.active ? "checked" : "") + ">" +
+                                                       "<input class=\"visible\" type=\"checkbox\" " + (px_value.active ? "checked" : "") + ">" +
+                                                       "</li>" );
             });
 
             $( "#mecsim_simulationlayer" ).selectable();
+            $( "#mecsim_simulationlayer .active" ).bootstrapSwitch({ size : "mini", "onText" : "active", "offText" : "inactive" });
+            $( "#mecsim_simulationlayer .visible" ).bootstrapSwitch({ size : "mini", "onText" : "visible", "offText" : "invisible" });
         }
     });
 

7e84f90220216c0d467b7834e5490505d7374f55
Philipp Kraus <pkraus@imac.local>
2015-04-26 02:42:52 +0200

    distribution

---
+++ src/main/java/de/tu_clausthal/in/mec/object/source/generator/CTimeDistribution.java
@@ -0,0 +1,74 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package de.tu_clausthal.in.mec.object.source.generator;
+
+
+import org.apache.commons.math3.distribution.AbstractRealDistribution;
+
+
+/**
+ * generator of a static number of objects with a time-exponential function
+ */
+public abstract class CTimeDistribution implements IGenerator
+{
+    /**
+     * distribution *
+     */
+    private final AbstractRealDistribution m_distribution;
+    /**
+     * number of cars *
+     */
+    private final int m_numberofcars;
+
+
+    /**
+     * default ctor
+     */
+    private CTimeDistribution()
+    {
+        m_distribution = null;
+        m_numberofcars = 0;
+    }
+
+    /**
+     * ctor
+     *
+     * @param p_distribution distribution object
+     * @param p_numberofcars number of cars
+     */
+    protected CTimeDistribution( final AbstractRealDistribution p_distribution, final int p_numberofcars )
+    {
+        m_numberofcars = p_numberofcars;
+        m_distribution = p_distribution;
+    }
+
+
+    @Override
+    public int generate( final int p_currentStep )
+    {
+        if ( m_distribution == null )
+            return 0;
+        return m_distribution.sample() <= 0.5 ? 0 : m_numberofcars;
+    }
+}
---
+++ src/main/java/de/tu_clausthal/in/mec/object/source/generator/CTimeExponentialDistribution.java
@@ -0,0 +1,47 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package de.tu_clausthal.in.mec.object.source.generator;
+
+import org.apache.commons.math3.distribution.ExponentialDistribution;
+
+
+/**
+ * creates a time-exponential distribution generator
+ */
+public class CTimeExponentialDistribution extends CTimeDistribution
+{
+
+    /**
+     * ctor
+     *
+     * @param p_numberofcars number of cars
+     * @param p_mean mean value
+     * @param p_deviation deviation value
+     */
+    public CTimeExponentialDistribution( final int p_numberofcars, final double p_mean, final double p_deviation )
+    {
+        super( new ExponentialDistribution( p_mean, p_deviation ), p_numberofcars );
+    }
+
+}

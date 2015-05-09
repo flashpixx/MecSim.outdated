1f4ba6294b290e992acd180ff581a0fa5519edaa
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-05-10 00:41:14 +0200

    agend binding

--- src/main/java/de/tu_clausthal/in/mec/object/car/CCarJasonAgent.java
+++ src/main/java/de/tu_clausthal/in/mec/object/car/CCarJasonAgent.java
@@ -49,7 +49,10 @@ import java.util.Set;
  */
 public class CCarJasonAgent extends CDefaultCar
 {
-
+    /**
+     * bind name
+     */
+    private static final String s_bindname = "self";
     /**
      * list of forbidden names *
      */
@@ -71,7 +74,7 @@ public class CCarJasonAgent extends CDefaultCar
     /**
      * agent object *
      */
-    private final CAgent<CDefaultCar> m_agent;
+    private final Set<CAgent<CDefaultCar>> m_agents = new HashSet<>();
     /**
      * inspector map
      */
@@ -79,7 +82,6 @@ public class CCarJasonAgent extends CDefaultCar
     {{
             putAll( CCarJasonAgent.super.inspect() );
         }};
-
 
     /**
      * @param p_route driving route
@@ -94,46 +96,89 @@ public class CCarJasonAgent extends CDefaultCar
     public CCarJasonAgent( final ArrayList<Pair<EdgeIteratorState, Integer>> p_route, final int p_speed, final int p_maxspeed, final int p_deceleration,
                            final int p_acceleration, final double p_lingerprobability, final String p_agent
     ) throws JasonException
+
+    {
+        this(
+                p_route, p_speed, p_maxspeed, p_deceleration, p_acceleration, p_lingerprobability, new HashMap<String, String>()
+        {{
+                put( "global", p_agent );
+            }}
+        );
+    }
+
+
+    /**
+     * @param p_route driving route
+     * @param p_speed initial speed
+     * @param p_maxspeed maximum speed
+     * @param p_acceleration acceleration
+     * @param p_deceleration decceleration
+     * @param p_lingerprobability linger probability
+     * @param p_agent map with bind and ASL name
+     * @throws JasonException throws on Jason error
+     */
+    public CCarJasonAgent( final ArrayList<Pair<EdgeIteratorState, Integer>> p_route, final int p_speed, final int p_maxspeed, final int p_deceleration,
+                           final int p_acceleration, final double p_lingerprobability, final Map<String, String> p_agent
+    ) throws JasonException
     {
         super( p_route, p_speed, p_maxspeed, p_deceleration, p_acceleration, p_lingerprobability );
-        m_agent = new CAgent<>( new CPath( this.getClass().getSimpleName() + "@" + this.hashCode(), "agent" ), p_agent );
+        for ( Map.Entry<String, String> l_item : p_agent.entrySet() )
+            this.bind( l_item.getKey(), l_item.getValue() );
+    }
+
+
+    /**
+     * binds an agent with the name
+     *
+     * @param p_bind bind name
+     * @param p_asl ASL name
+     * @throws JasonException
+     */
+    private void bind( final String p_bind, final String p_asl ) throws JasonException
+    {
+
+        final CAgent<CDefaultCar> l_agent = new CAgent<>( new CPath( this.getClass().getSimpleName() + "@" + this.hashCode(), p_bind ), p_asl );
 
         // add the belief bind to the agent
         final de.tu_clausthal.in.mec.object.mas.jason.belief.CFieldBind l_belief = new de.tu_clausthal.in.mec.object.mas.jason.belief.CFieldBind(
-                "self", this, c_forbidden
+                s_bindname, this, c_forbidden
         );
-        m_agent.getBelief().add( l_belief );
+        l_agent.getBelief().add( l_belief );
 
         // add the method bind to the agent
-        final CMethodBind l_method = new CMethodBind( "self", this );
-        l_method.getForbidden( "self" ).addAll( c_forbidden );
-        m_agent.getActions().put( "invoke", l_method );
+        final CMethodBind l_method = new CMethodBind( s_bindname, this );
+        l_method.getForbidden( s_bindname ).addAll( c_forbidden );
+        l_agent.getActions().put( "invoke", l_method );
 
         // add the set bind to the agent
         final de.tu_clausthal.in.mec.object.mas.jason.action.CFieldBind l_set = new de.tu_clausthal.in.mec.object.mas.jason.action.CFieldBind(
-                "self", this, c_forbidden
+                s_bindname, this, c_forbidden
         );
-        m_agent.getActions().put( "set", l_set );
+        l_agent.getActions().put( "set", l_set );
 
-        m_inspect.put( CCommon.getResourceString( this, "asl" ), m_agent.getSource() );
-        m_inspect.put( CCommon.getResourceString( this, "agent" ), m_agent.getName() );
+        m_inspect.put( CCommon.getResourceString( this, "asl", p_bind ), l_agent.getSource() );
+        m_inspect.put( CCommon.getResourceString( this, "agent", p_bind ), l_agent.getName() );
 
-        // add agent to layer
-        CSimulation.getInstance().getWorld().<IMultiLayer>getTyped( "Jason Car Agents" ).add( m_agent );
+        // add agent to layer and internal set
+        CSimulation.getInstance().getWorld().<IMultiLayer>getTyped( "Jason Car Agents" ).add( l_agent );
+        m_agents.add( l_agent );
+
     }
 
     @Override
     public final void release()
     {
         super.release();
-        if ( m_agent != null )
-            m_agent.release();
+        for ( CAgent<CDefaultCar> l_agent : m_agents )
+        {
+            l_agent.release();
+            CSimulation.getInstance().getWorld().<IMultiLayer>getTyped( "Jason Car Agents" ).remove( l_agent );
+        }
     }
 
     @Override
     public final Map<String, Object> inspect()
     {
-        m_inspect.put( CCommon.getResourceString( this, "cycle" ), m_agent.getCycle() );
         return m_inspect;
     }
 
@@ -143,7 +188,6 @@ public class CCarJasonAgent extends CDefaultCar
         // check speed, because agent can modify the speed value and the value should always in range
         // [0,max-speed] other values are declared as final member
         m_speed = Math.min( Math.max( 0, m_speed ), m_maxspeed );
-
         super.step( p_currentstep, p_layer );
     }
 }

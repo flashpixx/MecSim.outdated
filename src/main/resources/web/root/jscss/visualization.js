0e85bd21f9cf9f392d24fc8b86ec13dfd2065777
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-05-18 22:51:24 +0200

    visualization data access

--- src/main/resources/web/root/jscss/visualization.js
+++ src/main/resources/web/root/jscss/visualization.js
@@ -27,7 +27,40 @@
  * visualization components
  **/
 var Visualization = (function (px_modul) {
+/*
+    var randomData = function() {
 
+        result = [];
+        names = [];
+
+        //generate some nodes
+        for(var h=0; h<75; h++)
+            names.push("Knoten#"+h);
+
+        for(var i=0; i<names.length; i++){
+
+            var sourcemode = Math.random() > 0.5 ? true: false;
+            randomTargets = [];
+
+            //add some targets to every node by random
+            for(var j=0; j<names.length; j++)
+                if(Math.random() < 0.015)
+                    randomTargets.push(names[j]);
+
+
+            //push the new node object to the result array
+            result.push({ name: names[i].trim(),
+                sourcemode: sourcemode,
+                lat: Math.random() * 180,
+                long: Math.random() * 90,
+                targets: randomTargets
+            });
+
+        }
+
+        return result;
+    }
+*/
 
     /**
      * creates a edge bundle with D3.JS
@@ -35,15 +68,19 @@ var Visualization = (function (px_modul) {
      * @param pc_element element in which the graph is added
      * @param po_options options of the graph
      **/
-    px_modul.HierarchicalEdgeBundle = function(pc_element, po_options)
+    px_modul.HierarchicalEdgeBundling = function(pc_element, po_options)
     {
         var lo_options   = po_options || {};
 
-        lo_options.size    = lo_options.size    || 800;
-        lo_options.id      = lo_options.id      || null;
-        lo_options.tension = lo_options.tension || 0.85;
-        lo_options.radius  = lo_options.radius  || function( po_data ) { return po_data.y; };
-        lo_options.angle   = lo_options.angle   || function( po_data ) { return po_data.x / 180 * Math.PI; }
+        lo_options.datanodes    = lo_options.datanodes    || []
+        lo_options.size         = lo_options.size         || 800;
+        lo_options.id           = lo_options.id           || null;
+        lo_options.tension      = lo_options.tension      || 0.85;
+        lo_options.radius       = lo_options.radius       || function( po_data ) { return po_data.y; };
+        lo_options.angle        = lo_options.angle        || function( po_data ) { return po_data.x / 180 * Math.PI; };
+        lo_options.nodeid       = lo_options.nodeid       || function( po_node ) { return "node-" + po_node.key; };
+        lo_options.nodefilter   = lo_options.nodefilter   || function( po_node ) { return !po_node.children; };
+        lo_options.nodetext     = lo_options.nodetext     || function( po_node ) { return po_node.key; };
 
         // graph bundle
         var lo_bundle = d3.layout.bundle();
@@ -67,6 +104,20 @@ var Visualization = (function (px_modul) {
             .attr("d", d3.svg.arc().outerRadius(lo_options.size/2 - 120).innerRadius(0).startAngle(0).endAngle(2 * Math.PI))
             //.on("mousedown", mousedown);
             //.attr("class", "arc")
+        lo_svg.selectAll("g.node")
+            .data( lo_options.datanodes.filter(lo_options.nodefilter) )
+            .enter().append("svg:g")
+            .attr("class", "node")
+            .attr("id", lo_options.nodeid)
+            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+            .append("svg:text")
+            .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
+            .attr("dy", ".31em")
+            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+            .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
+            .text(lo_options.nodetext);
+            //.on("mouseover", mouseover)
+            //.on("mouseout", mouseout);
 
         // add line structure
         var lo_line = d3.svg.line.radial()
@@ -74,6 +125,7 @@ var Visualization = (function (px_modul) {
             .tension(lo_options.tension)
             .radius(lo_options.radius)
             .angle(lo_options.angle);
+
 
     }
 

0c0670ca79f974abebf08d9f680c781949fd0f89
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-05-20 13:08:29 +0200

    documentation

--- src/main/resources/web/root/jscss/visualization.js
+++ src/main/resources/web/root/jscss/visualization.js
@@ -38,31 +38,44 @@ var Visualization = (function (px_modul) {
     {
         var lo_options   = po_options || {};
 
+        /** array with JSON nodes with the structure { key : "labelname", children : [list with key names]} **/
         lo_options.datanodes    = lo_options.datanodes    || [];
+        /** image size **/
         lo_options.size         = lo_options.size         || 1000;
+        /** size of the cluster (quarter of the size) **/
         lo_options.clustersize  = lo_options.clustersize  || lo_options.size/4;
-        lo_options.id           = lo_options.id           || null;
+        /** tension value **/
         lo_options.tension      = lo_options.tension      || 0.85;
+        /** DOM ID of the graphic **/
+        lo_options.id           = lo_options.id           || null;
+        /** DOM class of the arc **/
         lo_options.pathclass    = lo_options.pathclass    || "arc";
+        /** DOM class of the node **/
         lo_options.nodeclass    = lo_options.nodeclass    || "node";
 
+        /** data sorting function **/
         lo_options.sort         = lo_options.sort         || null;
+        /** radius function **/
         lo_options.radius       = lo_options.radius       || function( po_data ) { return po_data.y; };
+        /** angle function **/
         lo_options.angle        = lo_options.angle        || function( po_data ) { return po_data.x / 180 * Math.PI; };
 
-        lo_options.nodeid       = lo_options.nodeid       || function( po_node ) { return "node-" + po_node.key; };
+        /** function tp create DOM ID of the node **/
+        lo_options.nodeid       = lo_options.nodeid       || function( po_node ) { return (lo_options.id == null ? "" : lo_options.id+"-") + "node-" + po_node.key; };
+        /** function for filtering the nodes **/
         lo_options.nodefilter   = lo_options.nodefilter   || function( po_node ) { return !po_node.children; };
+        /** function to return the node text **/
         lo_options.nodetext     = lo_options.nodetext     || function( po_node ) { return po_node.key; };
 
 
-
+        // create cluster call (size = 360 degree, cluster size)
         var lo_cluster = d3.layout.cluster()
             .size( [360, lo_options.clustersize] )
             .sort( lo_options.sort );
 
         var lo_bundle = d3.layout.bundle();
 
-        var line = d3.svg.line.radial()
+        var lo_line = d3.svg.line.radial()
             .interpolate( "bundle" )
             .tension(     lo_options.tension )
             .radius(      lo_options.radius )
@@ -72,7 +85,6 @@ var Visualization = (function (px_modul) {
             .insert( "div" )
             .style(  "width",  lo_options.size + "px")
             .style(  "height", lo_options.size + "px")
-
         if (lo_options.id  != null)
             div.attr("id", lo_options.id);
 
@@ -86,6 +98,8 @@ var Visualization = (function (px_modul) {
             .attr("class", lo_options.pathclass)
             .attr("d",     d3.svg.arc().outerRadius(lo_options.size/2).innerRadius(0).startAngle(0).endAngle(2 * Math.PI) );
 
+
+        // set data
         d3.json( "http://mbostock.github.io/d3/talk/20111116/flare-imports.json", function(classes) {
 
             var nodes = lo_cluster.nodes(packages.root(classes)), links = packages.imports(nodes), splines = lo_bundle(links);
@@ -94,7 +108,7 @@ var Visualization = (function (px_modul) {
                 .data(links)
                 .enter().append("svg:path")
                 .attr("class", function(d) { return "link source-" + d.source.key + " target-" + d.target.key; })
-                .attr("d", function(d, i) { return line(splines[i]); });
+                .attr("d", function(d, i) { return lo_line(splines[i]); });
 
             svg.selectAll( "g.node" )
                 .data( nodes.filter( lo_options.nodefilter) )
@@ -104,10 +118,10 @@ var Visualization = (function (px_modul) {
                 .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
                 .append("svg:text")
                 .attr("dx", function(d) { return d.x < 180 ? 8 : -8; })
-                .attr("dy", ".31em")
+                .attr("dy", "0.5em")
                 .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-                .attr("transform", function(d) { return d.x < 180 ? null : "rotate(180)"; })
-                .text(function(d) { return d.key; });
+                .attr("transform",   function(d) { return d.x < 180 ? null : "rotate(180)"; })
+                .text( lo_options.nodetext );
 
         });
 

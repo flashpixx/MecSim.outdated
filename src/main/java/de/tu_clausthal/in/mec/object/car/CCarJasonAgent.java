3dd7aa5bd3630a71cb1768c68a98e2461d78ad29
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-05-29 15:47:48 +0200

    change unit

--- src/main/java/de/tu_clausthal/in/mec/object/car/CCarJasonAgent.java
+++ src/main/java/de/tu_clausthal/in/mec/object/car/CCarJasonAgent.java
@@ -166,10 +166,10 @@ public class CCarJasonAgent extends CDefaultCar implements ICycle
     }
 
     /**
-     * returns the predeccor with kilometer values
+     * returns the predeccor with meter values
      *
      * @param p_count number of predecessors
-     * @return distance and predecessor
+     * @return distance in meter and predecessor
      *
      * @todo should be represent with a sensor definition
      */
@@ -178,7 +178,7 @@ public class CCarJasonAgent extends CDefaultCar implements ICycle
         return new HashMap<Double, ICar>()
         {{
                 for ( final Map.Entry<Integer, ICar> l_item : getPredecessor( p_count ).entrySet() )
-                    put( m_graph.getCellDistanceToMeter( l_item.getKey() ) / 1000.0, l_item.getValue() );
+                    put( m_graph.getCellDistanceToMeter( l_item.getKey() ), l_item.getValue() );
             }};
     }
 

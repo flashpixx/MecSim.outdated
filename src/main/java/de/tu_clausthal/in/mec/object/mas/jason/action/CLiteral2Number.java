82e2c443901920e6cc92287980591eeeb2c3e012
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-05-29 15:28:01 +0200

    add literal2number

---
+++ src/main/java/de/tu_clausthal/in/mec/object/mas/jason/action/CLiteral2Number.java
@@ -0,0 +1,61 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+package de.tu_clausthal.in.mec.object.mas.jason.action;
+
+import jason.asSemantics.DefaultInternalAction;
+import jason.asSemantics.TransitionSystem;
+import jason.asSemantics.Unifier;
+import jason.asSyntax.NumberTerm;
+import jason.asSyntax.NumberTermImpl;
+import jason.asSyntax.Term;
+
+
+/**
+ * converts a Jason term into a Jason number term
+ */
+public class CLiteral2Number extends DefaultInternalAction
+{
+
+    @Override
+    public int getMinArgs()
+    {
+        return 2;
+    }
+
+    @Override
+    public int getMaxArgs()
+    {
+        return 2;
+    }
+
+    @Override
+    public Object execute( final TransitionSystem p_transitionsystem, final Unifier p_unifier, final Term[] p_args ) throws Exception
+    {
+        if ( !p_args[0].isNumeric() )
+            return false;
+
+        return p_unifier.unifies( p_args[1], new NumberTermImpl( ( (NumberTerm) p_args[0] ).solve() ) );
+    }
+
+}

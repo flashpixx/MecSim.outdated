732e08b4dbdf3bdae816f07c78806f7bdb635424
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-06-07 13:55:24 +0200

    add prototype

---
+++ src/main/resources/web/root/jscss/pane.js
@@ -0,0 +1,57 @@
+function Pane( pc_name )
+{
+    this.name = pc_name;
+}
+
+
+
+Pane.prototype.getName = function()
+{
+    return this.name;
+}
+
+Pane.prototype.generateID = function( pc_subname )
+{
+    return;
+}
+
+Pane.prototype.beforeShowContent = function()
+{
+}
+
+Pane.prototype.beforeHideContent = function()
+{
+}
+
+
+Pane.prototype.getMenuHeader = function()
+{
+}
+
+Pane.prototype.getMenuContent = function()
+{
+}
+
+Pane.prototype.getMenuCSS     = function()
+{
+}
+
+
+Pane.prototype.getMainContent = function()
+{
+}
+
+Pane.prototype.getMainCSS     = function()
+{
+}
+
+
+Pane.prototype.getGlobalContent = function()
+{
+}
+
+Pane.prototype.getGlobalCSS = function()
+{
+}
+
+

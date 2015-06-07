1200e2805d8d8b729ff43fc0929e5af3b30f49ef
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-06-07 23:32:46 +0200

    docu

--- src/main/resources/web/root/jscss/content/logger.js
+++ src/main/resources/web/root/jscss/content/logger.js
@@ -1,14 +1,47 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+
 "use strict";
 
+
+/**
+ * ctor to create the logger instance
+ *
+ * @param pc_id ID
+**/
 function Logger( pc_id )
 {
-    Pane.call(this, pc_id);
+    Pane.call(this, pc_id)
 }
 
+/** inheritance call **/
 Logger.prototype = Object.create(Pane.prototype);
 
 
-
+/**
+ * @Overwrite
+ **/
 Logger.prototype.getGlobalContent = function()
 {
     return "<div id=\"blub\">hallo</div>";
--- src/main/resources/web/root/jscss/pane.js
+++ src/main/resources/web/root/jscss/pane.js
@@ -1,3 +1,27 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+
 "use strict";
 
 // @todo http://tobyho.com/2010/11/22/javascript-constructors-and/
@@ -16,7 +40,7 @@ function Pane( pc_id )
     if (!classof(pc_id, "string"))
         throw "ID undefinied";
 
-    this.id = pc_id.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase();
+    this.mc_id = pc_id.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase();
 }
 
 
@@ -27,7 +51,7 @@ function Pane( pc_id )
 **/
 Pane.prototype.getID = function()
 {
-    return this.id;
+    return this.mc_id;
 }
 
 /**
@@ -41,16 +65,25 @@ Pane.prototype.generateSubID = function( pc_id )
     if (!classof(pc_id, "string"))
         throw "ID undefinied";
 
-    return ["mecsim", this.getID, pc_id.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase()].join("_");
+    return ["mecsim", this.mc_id, pc_id.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase()].join("_");
 }
 
 
-
+/**
+ * returns the HTML content for the body
+ *
+ * @return HTML string or null
+**/
 Pane.prototype.getGlobalContent = function()
 {
     return null;
 }
 
+/**
+ * returns the CSS content for the body
+ *
+ * @return CSS content
+**/
 Pane.prototype.getGlobalCSS = function()
 {
     return null;

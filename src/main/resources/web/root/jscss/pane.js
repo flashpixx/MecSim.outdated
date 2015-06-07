7cf84173df9338c226c3c518084dd630a1252bfb
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-06-07 15:52:14 +0200

    add base functions

--- src/main/resources/web/root/jscss/pane.js
+++ src/main/resources/web/root/jscss/pane.js
@@ -1,28 +1,72 @@
-function Pane( pc_name )
+/**
+ * ctor of a UI pane
+ *
+ * @param pc_id internal name of the id
+**/
+function Pane( pc_id )
 {
-    this.name = pc_name;
+    this.id = pc_id.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase();
+}
+
+
+/**
+ * returns the internal ID
+ *
+ * @return id
+**/
+Pane.prototype.getID = function()
+{
+    return this.id;
+}
+
+/**
+ * returns a subID of the current pane
+ *
+ * @param pc_subid name of the sub structure
+ * @return subID
+**/
+Pane.prototype.generateSubID = function( pc_subid )
+{
+    return ["mecsim", this.getID, pc_subid.replace(/[^a-z0-9]+|\s+/gmi, "").toLowerCase()].join("_");
 }
 
 
 
-Pane.prototype.getName = function()
-{
-    return this.name;
-}
-
-Pane.prototype.generateID = function( pc_subname )
-{
-    return;
-}
-
-Pane.prototype.beforeShowContent = function()
+Pane.prototype.getGlobalContent = function()
 {
 }
 
-Pane.prototype.beforeHideContent = function()
+Pane.prototype.getGlobalCSS = function()
 {
 }
 
+
+
+Pane.prototype.beforeShowMainContent = function()
+{
+}
+
+Pane.prototype.beforeHideMainContent = function()
+{
+}
+
+Pane.prototype.getMainContent = function()
+{
+}
+
+Pane.prototype.getMainCSS     = function()
+{
+}
+
+
+
+Pane.prototype.beforeOpenMenu = function()
+{
+}
+
+Pane.prototype.getMenuName = function()
+{
+}
 
 Pane.prototype.getMenuHeader = function()
 {
@@ -35,23 +79,3 @@ Pane.prototype.getMenuContent = function()
 Pane.prototype.getMenuCSS     = function()
 {
 }
-
-
-Pane.prototype.getMainContent = function()
-{
-}
-
-Pane.prototype.getMainCSS     = function()
-{
-}
-
-
-Pane.prototype.getGlobalContent = function()
-{
-}
-
-Pane.prototype.getGlobalCSS = function()
-{
-}
-
-

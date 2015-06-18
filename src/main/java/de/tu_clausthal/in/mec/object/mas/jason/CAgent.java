0dc555fdc23863df2eb15670f16a2615a3a0b10a
Marcel Spitzer <marcel.spitzer@tu-clausthal.de>
2015-06-18 17:08:34 +0200

    fixed agent

--- src/main/java/de/tu_clausthal/in/mec/object/mas/jason/CAgent.java
+++ src/main/java/de/tu_clausthal/in/mec/object/mas/jason/CAgent.java
@@ -279,7 +279,7 @@ public class CAgent<T> implements IVoidAgent
     @Override
     public void removeLiteral(final CPath p_path, final Object p_data)
     {
-        m_beliefs.get(p_path.getSubPath(0, p_path.size()-1)).getLiterals().remove(CCommon.convertGeneric(CCommon.getLiteral(p_path.getSuffix(), p_data)));
+        m_beliefs.get(p_path.getSubPath(0, p_path.size()-1)).getLiterals( CPath.EMPTY ).remove(CCommon.convertGeneric(CCommon.getLiteral(p_path.getSuffix(), p_data)));
     }
 
     /**
@@ -335,7 +335,7 @@ public class CAgent<T> implements IVoidAgent
     @Override
     public final void receiveMessage(final Set<IMessage> p_messages)
     {
-        ((CMessageBeliefBase) m_beliefs.getBeliefbases().get("messages")).receiveMessage(p_messages);
+        ((CMessageBeliefBase) m_beliefs.getBeliefbases( CPath.EMPTY ).get("messages")).receiveMessage(p_messages);
     }
 
     @Override
@@ -453,8 +453,8 @@ public class CAgent<T> implements IVoidAgent
 
             m_beliefs.getLiterals().addAll( CCommon.convertGeneric(m_agent.getBB()) );
 
-            for( final IBeliefBase<Literal> l_beliefbase : m_beliefs.getBeliefbases().values() )
-                m_beliefs.getLiterals().retainAll(l_beliefbase.getLiterals());
+            for( final IBeliefBase<Literal> l_beliefbase : m_beliefs.getBeliefbases( CPath.EMPTY ).values() )
+                m_beliefs.getLiterals().retainAll(l_beliefbase.getLiterals( CPath.EMPTY ));
 
             // run all register after-cycle object
             for (final ICycle l_item : m_cycleobject)

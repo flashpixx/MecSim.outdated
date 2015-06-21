3e529abea5341e14a5da2d8936204abc85d47095
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-06-21 21:32:10 +0200

    docu

--- src/main/java/de/tu_clausthal/in/mec/ui/web/CBrowser.java
+++ src/main/java/de/tu_clausthal/in/mec/ui/web/CBrowser.java
@@ -42,6 +42,7 @@ import javafx.scene.web.WebView;
  *
  * @see http://docs.oracle.com/javafx/2/webview/jfxpub-webview.htm
  * @see http://dammsebastian.blogspot.de/2012/06/javafx-20-layout-panes-gridpane.html
+ * @todo add popup handler https://docs.oracle.com/javase/8/javafx/embedded-browser-tutorial/pop-up.htm / https://docs.oracle.com/javase/8/javafx/embedded-browser-tutorial/overview.htm / http://controlsfx.bitbucket.org/org/controlsfx/control/PopOver.html
  */
 public class CBrowser extends GridPane
 {

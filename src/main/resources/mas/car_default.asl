b57bbcd3b9da9fa25b52d51eb8ab00d724f08406
Philipp Kraus <philipp.kraus@flashpixx.de>
2015-06-28 13:43:36 +0200

    add waypoint elements

---
+++ src/main/resources/mas/car_default.asl
@@ -0,0 +1,52 @@
+// acceleration (based on Nagel-Schreckenberg model)
++!accelerate
+   :  m_speed(Speed) &
+      m_acceleration(Accelerate) &
+      m_maxspeed(MaxSpeed)
+   <- .min([MaxSpeed, Speed+Accelerate], NewSpeed);
+      set(self, m_speed, NewSpeed);
+      !drive.
+
+// deceleration (based on Nagel-Schreckenberg model)
++!decelerate
+   :  m_speed(Speed) &
+      m_deceleration(Decelerate) &
+      Decelerate > 0
+   <- .max([5, Speed-Decelerate], NewSpeed);
+      set(self, m_speed, NewSpeed);
+      !drive.
+
+// driving call
++!drive
+    :    m_speed(Speed) &
+         m_deceleration(Deceleration) &
+	     not (.empty([Predecessor]))
+
+    <-   // get distance to predecessing car
+         Predecessor =.. [X|_];
+         mecsim.literal2number(X,Distance);
+
+	     // add the speed range
+         for ( .range(I, 1, math.ceil( Speed / 10 ) ) )
+         {
+             +speed_range(I * 10);
+         }
+
+	     // calculate braking distance with gaussian sum
+         UpperSumIndex = math.floor( Speed / Deceleration );
+         BrakingDistance = UpperSumIndex * ( Speed - 0.5 * Deceleration * ( UpperSumIndex + 1 ) );
+
+	 	// check if predecessing car is too close
+	 	if ( BrakingDistance > Scramble*Distance )
+    	 {
+		!decelerate;
+    	 }
+    	 else
+    	 {
+		!accelerate;
+	 }.
+
+// default behaviour - accelerate
++!drive
+   :  true
+   <- !accelerate.
---
+++ src/main/resources/web/root/jscss/content/traffic/waypointconnection.js
@@ -0,0 +1,43 @@
+/**
+ * @cond LICENSE
+ * ######################################################################################
+ * # GPL License                                                                        #
+ * #                                                                                    #
+ * # This file is part of the TUC Wirtschaftsinformatik - MecSim                        #
+ * # Copyright (c) 2014-15, Philipp Kraus (philipp.kraus@tu-clausthal.de)               #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU General Public License as                            #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU General Public License for more details.                                       #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU General Public License                  #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
+ */
+
+
+"use strict";
+
+//@todo fix HTML5 data attributes
+
+
+/**
+ * ctor to create the waypoint connection widget
+ *
+ * @param pc_id ID
+ * @param pc_name name of the panel
+ * @param pa_panel array with child elements
+**/
+function WaypointConnection( pc_id, pc_name, pa_panel )
+{
+    Widget.call(this, pc_id, pc_name, pa_panel, lo_options );
+}
+
+/** inheritance call **/
+WaypointConnection.prototype = Object.create(Widget.prototype);

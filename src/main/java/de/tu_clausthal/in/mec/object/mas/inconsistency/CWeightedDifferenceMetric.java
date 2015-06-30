8400b9caee2286a78e4912eceb57a64533622f22
Marcel Spitzer <marcel.spitzer@tu-clausthal.de>
2015-06-30 22:06:58 +0200

    fix

--- src/main/java/de/tu_clausthal/in/mec/object/mas/inconsistency/CWeightedDifferenceMetric.java
+++ src/main/java/de/tu_clausthal/in/mec/object/mas/inconsistency/CWeightedDifferenceMetric.java
@@ -8,41 +8,42 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * User: marcel
- * Date: 6/30/15
+ * Metric implementation for agents. Calculates the distance with respect
+ * to size of union and intersection of beliefbases.
  */
 public class CWeightedDifferenceMetric<T extends IAgent> extends IDefaultMetric<T>
 {
     @Override
-    public double calculate( T p_first, T p_second )
+    public double calculate( final T p_first, final T p_second )
     {
         // equal objects create zero value
         if ( p_first.equals( p_second ) )
             return 0;
 
+        // collect all literals within specified paths
         final Set<ILiteral<?>> l_firstLiterals = new HashSet<>();
         final Set<ILiteral<?>> l_secondLiterals = new HashSet<>();
-
         for ( final CPath l_path : m_paths )
         {
             l_firstLiterals.addAll( p_first.getBeliefs().getLiterals( l_path ) );
             l_secondLiterals.addAll( p_second.getBeliefs().getLiterals( l_path ) );
         }
 
+        // get size of union
         final Set<ILiteral<?>> l_set = new HashSet<ILiteral<?>>()
         {{
                 addAll( l_firstLiterals );
                 addAll( l_secondLiterals );
             }};
-
         final int l_unionSize = l_set.size();
 
+        // get size of intersection
         l_set.retainAll(l_firstLiterals);
         l_set.retainAll(l_secondLiterals);
-
         final int l_intersectionSize = l_set.size();
 
-        return new Double( ( ( l_set.size() - l_firstLiterals.size() ) +
-                             ( l_set.size() - l_secondLiterals.size() ) ) * l_unionSize / l_intersectionSize );
+        // return distance
+        return new Double( ( ( l_unionSize - l_firstLiterals.size() ) +
+                             ( l_unionSize - l_secondLiterals.size() ) ) * l_unionSize / l_intersectionSize );
     }
 }
